1d7fc17e2f68221ba98f754f39cfdde9bec44683
chidalgoj <chidalgoj@gmail.com>
2019-02-15 13:26:15 -0600

    New activities added

--- app/src/main/java/com/hjapps/sinpefacil/sinpefacil/MainActivity.java
+++ app/src/main/java/com/hjapps/sinpefacil/sinpefacil/MainActivity.java
@@ -13,8 +13,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void openAccountBalanceView(View view){
+        Intent intent = new Intent(this, AccountBalanceActivity.class);
+
+        startActivity(intent);
+
+    }
+
     public void openTransferView(View view){
         Intent intent = new Intent(this, TransferActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void openTopUpView(View view){
+        Intent intent = new Intent(this, TopUpActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void openLastMovementsView(View view){
+        Intent intent = new Intent(this, LastMovementsActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    public void openConfigView(View view){
+        Intent intent = new Intent(this, ConfigActivity.class);
 
         startActivity(intent);
 
